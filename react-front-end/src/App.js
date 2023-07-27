f5efbf91ad74206ace9571fb537b35d2edefc133
Oscar <omankiewicz77@gmail.com>
2023-07-27 14:22:33 -0400

    created useEffect and cleaned code

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -3,19 +3,36 @@ import classnames from 'classnames'
 import axios from 'axios';
 import './App.css';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect, useState, createContext, useContext } from 'react';
 
 //Import components
-//import Login from './Login';
-//import Logout from './Logout';
+import Login from './components/Login';
 import Nav from './components/Navigation'
 import Product from './components/Product';
-import { useEffect } from 'react/cjs/react.production.min';
+import Logout from './components/Logout';
 
+const userContext = createContext();
 
 function App() {
-    const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
+    const { isAuthenticated, user } = useAuth0();
+    const [userId, setUserId] = useState(null);
+
+    useEffect(() => {
+      if (isAuthenticated) {
+        axios.post('/api/users', user) //where is this endpoint?
+          .then(res => {
+            const userId = res.data.userId;
+            setUserId(userId);
+            window.sessionStorage.setItem('userId', userId);
+          })
+          .catch(err => {
+            console.log(err)
+          });
+      }
+    }, [isAuthenticated, user])
+    /*
     useEffect (() => {
-        //make a request to a back end route TBD
+        //make a request to a back end router
         //send over entire user object
         //backend query user object
         //if match found send data to front end
@@ -26,33 +43,20 @@ function App() {
         //save userid info in a useContext
         //second option windows.sessionStorage
     }, [isAuthenticated])
-    console.log(user)
-
-
-
-function App() {
-    const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
-    // console.log(user)
+    */
     if (isAuthenticated) {
       // Render content for authenticated users
       return (
         <div>
-          <h1>goodbye, {user.name}</h1>
-          <button onClick={() => logout()}>Logout</button>
-
+          <Logout />  
           <Product />
         </div>
       );
     } else {
       // Render content for non-authenticated users
       return (
-        <div>
-          <h1>Welcome! Please log in.</h1>
-          <button onClick={() => loginWithRedirect()}>Login</button>
-
-        </div>
+        <Login />
       );
     }
-}
 }
   export default App;
