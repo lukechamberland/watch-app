6e35272ab00790b159a6faf11dc77a3c03c6b5d5
Oscar <omankiewicz77@gmail.com>
2023-08-01 15:27:53 -0400

    logout deletes session

--- react-front-end/src/components/Logout.js
+++ react-front-end/src/components/Logout.js
@@ -3,11 +3,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const Logout = () => {
   const { user, logout } = useAuth0();
-
+ const userLogout = () => {
+    window.sessionStorage.removeItem("userId");
+    logout()
+ }
   return (
     <div>
       <h1>Hello, {user.name}</h1>
-      <button onClick={() => logout()}>Logout</button>
+      <button onClick={userLogout}>Logout</button>
     </div>
   );
 };
