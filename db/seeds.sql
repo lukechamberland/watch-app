4c50b356fa59cf8fb1dd8145e59e547d744aa35e
Sharleen Beniamin <sharleenb16@gmail.com>
2023-08-13 21:17:23 +0000

    merge seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,7 +8,25 @@ VALUES
   (7, 'https://timex.ca/cdn/shop/products/TW2V52500.png?v=1687301810&width=900', 'new watch 3', 'Rose Gold Watch', 220, 25, 'true', 'womens'),
   (7, 'https://www.bogartsjewellers.com/cdn/shop/products/Dress_1_400x500.png?v=1629373102', 'new watch 5', 'Silver Watch', 220, 25, 'true', 'mens'),
   (5, 'https://www.bell.ca/Styles/images/109452_Apple_Watch_SE2_40mm_Strlite_Sprt_Bnd_lrg1.png', 'Second Generation', 'Apple Watch SE', 250, 2, 'true', 'athletic'),
-  (7, 'https://www.nixon.com/cdn/shop/files/A1057-1031-view1_e9cc997a-e3cb-41e3-8269-711541168606.png?v=1684461002', 'All Black', 'Porter', 190, 4, 'true', 'mens');
+  (7, 'https://www.nixon.com/cdn/shop/files/A1057-1031-view1_e9cc997a-e3cb-41e3-8269-711541168606.png?v=1684461002', 'All Black', 'Porter', 190, 4, 'true', 'mens'),
+  (5, 'https://ca.nixon.com/cdn/shop/files/A1342-5087-view1.png?v=1684460837', 'Elegant stainless steel watch', 'Gold Stainless Steel Watch', 130, 3, 'true', 'womens'),
+  (7, 'https://ca.nixon.com/cdn/shop/files/A1342-1921-view1.png?v=1684460835', 'Elegant stainless steel watch', 'Silver Gold Stainless Steel Watch', 160, 3, 'true', 'womens'),
+  (6, 'https://static.wixstatic.com/media/e01a67_752b7e6ab5014e35abe7763d8947f790~mv2_d_2880_3780_s_4_2.png/v1/crop/x_0,y_144,w_2880,h_3492/fill/w_950,h_1152,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/e01a67_752b7e6ab5014e35abe7763d8947f790~mv2_d_2880_3780_s_4_2.png', 'Modern Black', 'Simpl Watch', 260, 6, 'true', 'womens'),
+  (7, 'https://obsessionsj.com/cdn/shop/products/97L146_6d4c500f-bb2a-4b82-9043-08b440bb3341_2048x.png?v=1620726604', 'Leather Band', 'Bulova Classic', 230, 4, 'true', 'womens'),
+  (1, 'https://obsessionsj.com/cdn/shop/products/BH1673-50E_43ccc6d6-c7b9-42d4-9411-41db728a4fec_2048x.png?v=1622548129', 'Rectangle Gold Watch', 'Citizen Quartz', 280, 4, 'true', 'mens'),
+  (3, 'https://obsessionsj.com/cdn/shop/products/98C123_941ebe85-39e1-47fb-870d-fe63b541c8a2_2048x.png?v=1620726548', 'Stainless Steel', 'Bulova Futuro', 300, 2, 'true', 'mens'),
+  (4, 'https://obsessionsj.com/cdn/shop/products/BH1673-50E_43ccc6d6-c7b9-42d4-9411-41db728a4fec_2048x.png?v=1622548129', 'Rectangle Gold Watch', 'Citizen Quartz', 280, 4, 'true', 'mens'),
+  (7, 'https://halifaxwatch.com/cdn/shop/products/AW1598-70X_2048x.png?v=1549669310', 'Green Dial', 'Citizen Eco-Drive', 190, 2, 'true', 'mens'),
+  (1, 'https://s3-ap-southeast-1.amazonaws.com/habib-group-media/ice-watch/latest-products/1500/ice-forever-blue-1.png', 'Blue', 'Ice Forever', 90, 6, 'true', 'kids'),
+  (2, 'https://s3-ap-southeast-1.amazonaws.com/habib-group-media/ice-watch/latest-products/1500/ice-forever-green-1.png', 'Green', 'Ice Forever', 90, 4, 'true', 'kids'),
+  (3, 'https://easytot.com/cdn/shop/products/914f90ff01dcf4e03a5c676d57c5271c62b058569766fce07b761496a613fd33.png?v=1671872799', 'Watermint Green', 'Millow Watch', 80, 2, 'true', 'kids'),
+  (4, 'https://timex.ca/cdn/shop/products/T89001.png?v=1687300227&width=900', 'Butterflies Fabric Watch', 'Timex Time Machine', 40, 3, 'true', 'kids'),
+  (5, 'https://gmtlondon.com/media/catalog/product/cache/2009becea5014db99ed0c5db2747b4cb/g/k/gk0015-01.png', 'Pink Watch', 'The Dazzling', 30, 4, 'true', 'kids'),
+  (2, 'https://www.jumpplus.com/web/image/product.product/29229/image_1024/%5BNM01133285%5D%20Nomad%20Sport%20Waterproof%20Band%20for%20Apple%20Watch%2042-44-45mm%20-%20Marine%20Blue?unique=8d7f879', 'Waterproof', 'Nomad Sport Apple Watch', 100, 40, 'true', 'athletic'),
+  (3, 'https://ca.nixon.com/cdn/shop/files/A1320-001-view1.png?v=1684460763', 'Black Band', 'Heat Sports Watch', 180, 2, 'true', 'athletic'),
+  (4, 'https://ca.nixon.com/cdn/shop/files/A1320-5106-view1.png?v=1684460781', 'Grey Band', 'Heat Sports Watch', 175, 3, 'true', 'athletic'),
+  (5, 'https://consumer.huawei.com/content/dam/huawei-cbg-site/common/mkt/pdp/wearables/watch-3/img/id/huawei-watch-3-strap-3-1.png', 'Black Sports Watch', 'Huawei Watch 3', 230, 4, 'true', 'athletic');
+
 
 INSERT INTO order_products (product_id, quantity, order_id)
 VALUES
